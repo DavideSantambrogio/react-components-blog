58edd2fb2c6a108231991dcd4c00905f3066616e
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-06-12 16:28:38 +0200

    add dynamic data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,22 @@
+import React from "react";
 import styles from "../style/Main.module.css";
+import data from "../../data/data.json";
 
 export default () => {
     return (
         <>
             <main className={styles.container}>
-                <div className={styles.card}>
-                    <img className={styles.image} src="public\images\600x400.jpg" alt="" />
-                    <div className={styles.txtContainer}>
-                        <h3>Titolo del post</h3>
-                        <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum itaque iste repudiandae accusantium repellendus dolorum dolor exercitationem ipsum, ipsa cumque distinctio delectus, minus consequuntur soluta harum totam temporibus rerum reprehenderit?</p>
-                        <div className={styles.button}>LEGGI DI PIÙ </div>
+                {data.map((item) => (
+                    <div key={item.id} className={styles.card}>
+                        <img className={styles.image} src={item.img} alt={`immagine di ${item.title}`} />
+                        <div className={styles.txtContainer}>
+                            <h3>{item.title}</h3>
+                            <p className={styles.description}>{item.description}</p>
+                            <div className={styles.button}>LEGGI DI PIÙ</div>
+                        </div>
                     </div>
-
-                </div>
+                ))}
             </main>
         </>
-    )
+    );
 }
